fd8e8d8816ef24c30d608f4dbb8d1e42fd5539b0
Rhoton <drhoto200@cable.comcast.com>
2020-04-22 13:26:47 -0400

    Highlighted Space Debris

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,11 +18,8 @@ export class AppComponent {
        response.json().then(function(data) {
  
           let fetchedSatellites = data.satellites;
-          // TODO: loop over satellites
           for(let i = 0; i < fetchedSatellites.length; i ++) {
-            // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
             let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational)
-            // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
             this.sourceList.push(satellite);   
                
            }
